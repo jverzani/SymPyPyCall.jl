fc72fe40dea6fa73b752bd60f252b043e21136cb
jverzani <jverzani@gmail.com>
2023-10-16 11:34:09 -0400

    cleanup

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # SymPyPyCall
 
+[![Docs](https://img.shields.io/badge/docs-dev-blue.svg)](https://jverzani.github.io/SymPyPythonCore.jl/dev)
 [![Build Status](https://github.com/jverzani/SymPyPyCall.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/jverzani/SymPyPyCall.jl/actions/workflows/CI.yml?query=branch%3Amain)
+[![Coverage](https://codecov.io/gh/jverzani/SymPyPyCall.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/jverzani/SymPyPyCall.jl)
+
+
+[SymPyCore](https://github.com/jverzani/SymPyCore.jl) provides a `Julia`n interface to the [SymPy](https://www.sympy.org/) library of Python.
+
+
+`SymPyPyCall`  utilizes `SymPyCore` and the `PyCall` package (to provide the interop between `Julia` and `Python`) to enable access to Python's SymPy library using the practices and idioms of `Julia`.
+
+The package [SymPyPythonCall](https://github.com/jverzani/SymPyPythonCall.jl) does a similar thing with the `PythonCall` package providing the interop.
+
+!!! note
+    It is anticipated that this package will be renamed `SymPy` at the next breaking change of the [SymPy](https://github.com/JuliaPy/SymPy.jl) package. As such, it is not expected that this package will be registered. To install this package, use `Pkg.add(PackageSpec(url="https://github.com/jverzani/SymPyPyCall.jl"))`.
+
+
+## Installation
+
+Installing this package should install the `SymPyCore` package and the `PyCall` package. The `PyCall` package installs a `Conda` package that installs the underlying SymPy library for Python.
--- src/python_connection.jl
+++ src/python_connection.jl
@@ -1,4 +1,7 @@
 ## PyCall specific usage
+Base.convert(::Type{S}, x::Sym{T}) where {T<:PyCall.PyObject, S<:Sym} = x
+Base.convert(::Type{S}, x::T) where {T<:PyCall.PyObject, S <: SymbolicObject} = Sym(x)
+
 SymPyCore._convert(::Type{T}, x) where {T} = convert(T, x)
 
 
@@ -7,7 +10,24 @@ Sym(x::Nothing) = Sym(PyCall.PyObject(nothing))
 SymPyCore.:↓(x::PyCall.PyObject) = x
 SymPyCore.:↓(d::Dict) = Dict(↓(k) => ↓(v) for (k,v) ∈ pairs(d))
 SymPyCore.:↓(x::Set) = _sympy_.sets.FiniteSet((↓(xi) for xi ∈ x)...)
+
 SymPyCore.:↑(::Type{<:AbstractString}, x) = PyObject(x)
+# Create a symbolic type. There are various containers to recurse in to be
+# caught here
+function SymPyCore.:↑(::Type{PyCall.PyObject}, x)
+    class_nm = SymPyCore.classname(x)
+    class_nm == "set"   && return Set(Sym.(collect(x)))
+    class_nm == "tuple" && return Tuple(↑(xᵢ) for xᵢ ∈ x)
+    class_nm == "list"  && return [↑(xᵢ) for xᵢ ∈ x]
+    class_nm == "dict"  && return Dict(↑(k) => ↑(x[k]) for k ∈ x)
+
+    class_nm == "FiniteSet" && return Set(Sym.(collect(x)))
+    class_nm == "MutableDenseMatrix" && return _up_matrix(x) #map(↑, x.tolist())
+
+    # others ... more hands on than pytype_mapping
+
+    Sym(x)
+end
 
 # ↑ for matrices
 _up_matrix(m) = map(↑, m.tolist())
@@ -48,11 +68,11 @@ function Base.getproperty(x::SymbolicObject{T}, a::Symbol) where {T <: PyCall.Py
         end
 
         if hasproperty(meth, :__call__)
-            meth = getproperty(meth, "__call__")
+           # meth = getproperty(meth, "__call__")
 
             return SymPyCore.SymbolicCallable(meth)
         end
-        return Sym(convert(PyCall.PyAny, meth))
+        return ↑(convert(PyCall.PyAny, meth))
     end
     # nothing?
     nothing
